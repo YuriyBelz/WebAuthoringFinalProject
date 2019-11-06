3a7502af3bfe134930b3d3377dd7a8fa92028534
YuriyBelz <yuriybelz1@gmail.com>
2019-11-05 19:59:56 -0500

    started adding js fles to refine the search

---
+++ scripts/searchRefine/reset.js
@@ -0,0 +1,5 @@
+for(i= 0; i <= document.getElementsByClassName("bookView").length; i++){
+  document.getElementsByClassName("bookView")[i].style.display = "block";
+}
+//this file is to reset the books by setting them all to block display/ default
+// it would make any hidden elements appear
