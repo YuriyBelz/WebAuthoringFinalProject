4ab712096317ce5d9f2de28d08cc08d003fe64ce
YuriyBelz <43324718+YuriyBelz@users.noreply.github.com>
2019-11-06 10:36:40 -0500

    Update discount.js

--- scripts/searchRefine/discount.js
+++ scripts/searchRefine/discount.js
@@ -3,4 +3,4 @@ for(var i= 0; i <= document.getElementsByClassName("bookView").length; i++){
     document.getElementsByClassName("bookView")[i].style.display = "none";
   }
 }
-//this file is hide all the bookview divs that arent child because we want to refine to only show the child books
+//this file is hide all the bookview divs that arent discount
