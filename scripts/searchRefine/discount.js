31a79dde6bf58e33ec17bc4874b52643295014b6
YuriyBelz <yuriybelz1@gmail.com>
2019-11-05 20:50:10 -0500

    added scripts for the bookrefine, they dont work though

---
+++ scripts/searchRefine/discount.js
@@ -0,0 +1,6 @@
+for(var i= 0; i <= document.getElementsByClassName("bookView").length; i++){
+  if (!= document.getElementsByClassName("bookView")[i].contains("nonfic"){
+    document.getElementsByClassName("bookView")[i].style.display = none;
+  }
+}
+//this file is hide all the bookview divs that arent child because we want to refine to only show the child books
