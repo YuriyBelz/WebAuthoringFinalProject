de26c1043a613f18f8c741eb9bedf2e1b73a7b04
YuriyBelz <yuriybelz1@gmail.com>
2019-11-15 23:43:46 -0500

    added a page for specific books, it doesnt work right now because i cant figure out how cokies work

---
+++ scripts/individualbooks.js
@@ -0,0 +1,12 @@
+/*this should execute when the page initially loads getting the value
+of the book that you want to display from the cookie and setting
+it to display*/
+document.getElementById("individualbookheader").textContent += document.cookie.chosenLink;
+document.getElementsByClassName("bookviewlarge")[document.cookie.chosenLink].style.display = "show";
+
+/*this function will add to the cart
+whenever the button is pressed the element on the array corresponding to 
+the book on the array will increment by one*/
+function addToCart(bookId){
+    document.cookie.cart[bookId]++;
+}
